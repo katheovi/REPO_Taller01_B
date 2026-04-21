75c77b90b9e72dcea98065011f464a91f6b11614
katheovi <katherinovi13@gmail.com>
2026-04-21 14:15:27 -0400

    REPOSITORIO CORRECTO

---
+++ Kathe_TallerGit_2b/REPO_Taller01_SeccionB/PersitenciaWAsa01/PersitenciaWAsa01/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: David Calles
+ * Fecha: 17/4/2026
+ * Hora: 2:16 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.IO;
+
+namespace PersitenciaWAsa01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Epa Chamo qlq este es el taller seccion B");
+			
+			// directorio
+			
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"DatosIujo");
+			
+			string rutaReportes = Path.Combine(rutaRaiz, "REPORTES");
+			
+			Console.Write(rutaRaiz);
+			Console.WriteLine(rutaReportes);
+			
+			if(!Directory.Exists(rutaReportes)){
+			   	Directory.CreateDirectory(rutaReportes);
+			   	Console.WriteLine("Directorio Creado Correctamente");
+			   }
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ Kathe_TallerGit_2b/REPO_Taller01_SeccionB/paso 1/kathe_TallerGit_2b/PersitenciaWAsa01/PersitenciaWAsa01/Program.cs
@@ -0,0 +1,51 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: KATHERIN OVIEDO
+ * Fecha: 17/4/2026
+ */
+using System;
+using System.IO;
+
+namespace PersitenciaWAsa01
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("taller seccion B");
+			
+			// --- CONFIGURACIÓN DE RUTAS ---
+			string rutaRaiz = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "DatosIujo");
+			string rutaReportes = Path.Combine(rutaRaiz, "REPORTES");
+			
+			if(!Directory.Exists(rutaReportes)){
+				Directory.CreateDirectory(rutaReportes);
+				Console.WriteLine("Directorio Creado Correctamente");
+			}
+
+			// --- DESAFÍO 1: EL VALIDADOR DE SEGURIDAD ---
+			// Paso 1: Recibir la cadena
+			string entradaUsuario = "admin;clave123"; 
+			
+			// Paso 2: Usar .Split(';') como dice la ayuda
+			string[] partes = entradaUsuario.Split(';');
+			string clave = partes[1];
+			
+			// Paso 3: Verificar si contiene "123"
+			if (clave.Contains("123")) {
+				// IMPORTANTE: Definimos la ruta del ARCHIVO, no de la carpeta
+				string rutaArchivoSeguridad = Path.Combine(rutaReportes, "seguridad.txt");
+				
+				using (StreamWriter sw = new StreamWriter(rutaArchivoSeguridad, true)) {
+					sw.WriteLine("Clave Débil detectada");
+				}
+				Console.WriteLine("> Desafío 1: Alerta de seguridad guardada.");
+			}
+
+			
+			Console.WriteLine("\n=== PROCESO FINALIZADO. ===");
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+} 
